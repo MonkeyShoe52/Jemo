3640a80a0d97bd90203b4632c53b44ef6173fe2c
jadenwilliams <jaw98x@gmail.com>
2023-12-11 13:54:52 -0500

    gghgh

--- src/main/java/com/example/jemo/HelloController.java
+++ src/main/java/com/example/jemo/HelloController.java
@@ -3,16 +3,19 @@ package com.example.jemo;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.collections.ObservableList;
 
 public class HelloController {
  // Fields for data
  // Fields for your controls
 
- public Button nextButton;
- public Button prevButton;
+ public Button CompleteButton;
+ public Button BringBackButton;
  public Button newButton;
  public Label label;
  public ListView listView;
+ public ListView listView2;
+ public ListView listView3;
 public TextField textField;
  public Button DeleteButton;
 
@@ -22,9 +25,28 @@ public TextField textField;
  }
 
   public void onActiondelete() {
-   String whatTheUserTyped = textField.getText();
-   listView.getItems().remove(whatTheUserTyped);
+   ObservableList selectedIndices = listView.getSelectionModel().getSelectedItems();
+   for (Object o : selectedIndices) {
+    listView.getItems().remove(o);
+    listView2.getItems().add(o);
+   }
   }
+
+ public void onActionBringBack() {
+  ObservableList selectedIndices = listView2.getSelectionModel().getSelectedItems();
+  for (Object o : selectedIndices) {
+   listView2.getItems().remove(o);
+   listView.getItems().add(o);
+  }
+ }
+
+ public void onActionComplete() {
+  ObservableList selectedIndices = listView.getSelectionModel().getSelectedItems();
+  for (Object o : selectedIndices) {
+   listView.getItems().remove(o);
+   listView3.getItems().add(o);
+  }
+ }
 
 
 
